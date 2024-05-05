166fd0bfbe5e34a61b251b30eaca7004fe4a86c2
mktinfortech <contato@mktinfortech.com.br>
2024-05-05 10:11:00 -0300

    Creating file tsconfig.json on the project

--- dist/main.js
+++ dist/main.js
@@ -1,5 +1,5 @@
 "use strict";
-exports.__esModule = true;
+Object.defineProperty(exports, "__esModule", { value: true });
 exports.multiplicar = void 0;
 function multiplicar(a, b) {
     return a * b;
