febb6ae8fb37d35a3069ab24b612d89b15bffbce
mktinfortech <contato@mktinfortech.com.br>
2024-05-05 09:46:26 -0300

    First Commit

---
+++ main.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+exports.multiplicar = void 0;
+function multiplicar(a, b) {
+    return a * b;
+}
+exports.multiplicar = multiplicar;
+console.log(multiplicar(7, 7));
---
+++ main.ts
@@ -0,0 +1,6 @@
+
+export function multiplicar(a:number, b:number) : number {
+    return a * b;
+}
+
+console.log(multiplicar(7 , 7));
